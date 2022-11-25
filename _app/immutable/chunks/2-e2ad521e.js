f0a974cf63d3a15edb87932cd5e3c3bd15772473
Rahel Piip <rahelini@gmail.com>
2022-11-25 20:32:21 +0200

    Update to svelte

---
+++ _app/immutable/chunks/2-e2ad521e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b0c84094.js";export{t as component};
---
+++ _app/immutable/chunks/3-f3c8f4ee.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/guide/_page.svelte-44a6ae36.js";export{t as component};
